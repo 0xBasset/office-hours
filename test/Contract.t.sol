9f8099e874e5e203ff58da737697cb1f22fab231
0xBasset <94132719+0xBasset@users.noreply.github.com>
2022-04-17 19:44:18 -0300

    Initial commit

---
+++ test/Contract.t.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.7;
+
+import "./utils/test.sol";
+import "./utils/Interfaces.sol";
+
+contract ContractTest is DSTest {
+        
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+    function setUp() public {}
+
+    function testExample() public {
+        assertTrue(true);
+    }
+}
